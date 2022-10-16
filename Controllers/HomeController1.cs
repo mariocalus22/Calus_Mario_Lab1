c22a90b83f1bc1e11ddc7395e30234aba6e3c54e
john <54267902+mariocalus22@users.noreply.github.com>
2022-10-16 22:26:57 +0300

    methods in controller

---
+++ Controllers/HomeController1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Calus_Mario_Lab1.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public string Index()
+        {
+            return "Welcome!";
+        }
+
+        public string Greeting()
+        {
+            return "Hello dear user!";
+        }
+
+        public string personalData(String name, int age)
+        {
+            return name + "your age is " + age;
+        }
+    }
+}
